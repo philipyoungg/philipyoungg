a91e59769ccce9b4e690b067f9a4a51f771d5ae4
Philip Young <philip.young.gunawan@gmail.com>
2017-03-09 13:45:46 +0700

    add book and thoughts

---
+++ books.js
@@ -0,0 +1,301 @@
+[{
+    "mustRead": 1,
+    "title": "The 4-Hour Workweek: Escape 9-5, Live Anywhere, and Join the New Rich",
+    "author": "Timothy Ferris",
+    "category": "Self-improvement",
+    "ISBN": 9780307465351,
+    "cover": "covers.openlibrary.org/b/isbn/9780307465351-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Tools of Titans: The Tactics, Routines, and Habits of Billionaires, Icons, and World-Class Performers",
+    "author": "Timothy Ferris",
+    "category": "Biography",
+    "ISBN": 1328683788,
+    "cover": "covers.openlibrary.org/b/isbn/1328683788-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Rock Breaks Scissors",
+    "author": "William Poundstone",
+    "category": "Statistics",
+    "ISBN": 1491531185,
+    "cover": "covers.openlibrary.org/b/isbn/1491531185-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Are You Smart Enough to Work at Google?",
+    "author": "William Poundstone",
+    "category": "Interviewing",
+    "ISBN": 316099988,
+    "cover": "covers.openlibrary.org/b/isbn/316099988-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "7 Habits of Highly Effective People",
+    "author": "Stephen R. Covey",
+    "category": "Self-improvement",
+    "ISBN": 9781511317290,
+    "cover": "covers.openlibrary.org/b/isbn/9781511317290-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "How to Win Friends and Influence People",
+    "author": "Dale Carnegie",
+    "category": "Self-improvement",
+    "ISBN": 671027034,
+    "cover": "covers.openlibrary.org/b/isbn/671027034-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Elon Musk",
+    "author": "Ashley Vance",
+    "category": "Biography",
+    "ISBN": 62301233,
+    "cover": "covers.openlibrary.org/b/isbn/62301233-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Thinking Fast and Slow",
+    "author": "Daniel Kahneman",
+    "category": "Psychology",
+    "ISBN": 374533555,
+    "cover": "covers.openlibrary.org/b/isbn/374533555-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Wait: The Art and Science of Delay",
+    "author": "Frank Partnoy",
+    "category": "Psychology",
+    "ISBN": 9781610392471,
+    "cover": "covers.openlibrary.org/b/isbn/9781610392471-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "The Cather In The Rye",
+    "author": "J.D. Salinger",
+    "category": "Fiction",
+    "ISBN": 316769487,
+    "cover": "covers.openlibrary.org/b/isbn/316769487-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Colorless Tsukuru Tazaki and His Years of Pilgrimage",
+    "author": "Haruki Murakami",
+    "category": "Fiction",
+    "ISBN": 804170126,
+    "cover": "covers.openlibrary.org/b/isbn/804170126-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Eragon (4 Books)",
+    "author": "Christopher Paolini",
+    "category": "Fiction",
+    "ISBN": 375826696,
+    "cover": "covers.openlibrary.org/b/isbn/375826696-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Twilight (4 Books)",
+    "author": "Stephanie Meyer",
+    "category": "Fiction",
+    "ISBN": 316015849,
+    "cover": "covers.openlibrary.org/b/isbn/316015849-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "The Alchemist",
+    "author": "Paulo Coelho",
+    "category": "Fiction",
+    "ISBN": 61122416,
+    "cover": "covers.openlibrary.org/b/isbn/61122416-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Manuscript Found in Accra",
+    "author": "Paulo Coelho",
+    "category": "Fiction",
+    "ISBN": 345805054,
+    "cover": "covers.openlibrary.org/b/isbn/345805054-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Eat, Pray, Love: One Woman's Search for Everything Across Italy, India and Indonesia",
+    "author": "Elizabeth Gilbert",
+    "category": "Travel Writing",
+    "ISBN": 9780747585664,
+    "cover": "covers.openlibrary.org/b/isbn/9780747585664-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "The Art of Woo: Using Strategic Persuasion to Sell Your Ideas",
+    "author": "G. Richard Shell",
+    "category": "Psychology",
+    "ISBN": 9780143114048,
+    "cover": "covers.openlibrary.org/b/isbn/9780143114048-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Contagious: Why Things Catch On",
+    "author": "Jonah Berger",
+    "category": "Psychology",
+    "ISBN": 9781451686586,
+    "cover": "covers.openlibrary.org/b/isbn/9781451686586-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Made to Stick",
+    "author": "Chip Heath and Dan Heath",
+    "category": "Psychology",
+    "ISBN": 1400064287,
+    "cover": "covers.openlibrary.org/b/isbn/1400064287-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Outliers: The Story of Success",
+    "author": "Malcolm Gladwell",
+    "category": "Statistics",
+    "ISBN": 316017930,
+    "cover": "covers.openlibrary.org/b/isbn/316017930-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Tribes: We Need You to Lead Us",
+    "author": "Malcolm Gladwell",
+    "category": "Business",
+    "ISBN": 1491514736,
+    "cover": "covers.openlibrary.org/b/isbn/1491514736-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Business Model Generation: A Handbook for Visionaries, Game Changers, and Challengers",
+    "author": "Alexander Osterwalder and Yves Pigneur",
+    "category": "Business",
+    "ISBN": 470876417,
+    "cover": "covers.openlibrary.org/b/isbn/470876417-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "The $100 Startup: Reinvent the Way You Make a Living, Do What You Love, and Create a New Future",
+    "author": "Chris Guillebeau",
+    "category": "Business",
+    "ISBN": 307951529,
+    "cover": "covers.openlibrary.org/b/isbn/307951529-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "Traction: How Any Startup Can Achieve Explosive Customer Growth",
+    "author": "Gabriel Weinberg and Justin Mares",
+    "category": "Business",
+    "ISBN": 1936661837,
+    "cover": "covers.openlibrary.org/b/isbn/1936661837-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "The Alchemyst: The Secrets of the Immortal Nicholas Flamel (6 Books)",
+    "author": "Michael Scott",
+    "category": "Fiction",
+    "ISBN": 385736002,
+    "cover": "covers.openlibrary.org/b/isbn/385736002-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "The Power of Habit: Why We Do What We Do in Life",
+    "author": "Charless Duhigg",
+    "category": "Self-improvement",
+    "ISBN": 8601406381322,
+    "cover": "covers.openlibrary.org/b/isbn/8601406381322-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "Get Some Headspace",
+    "author": "Andy Pudicombe",
+    "category": "Meditation",
+    "ISBN": 9781250104908,
+    "cover": "covers.openlibrary.org/b/isbn/9781250104908-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "The Power of Now: A Guide to Spiritual Enlightenment",
+    "author": "Eckhart Tolle",
+    "category": "Meditation",
+    "ISBN": 9781577314806,
+    "cover": "covers.openlibrary.org/b/isbn/9781577314806-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 0,
+    "title": "The Tipping Point",
+    "author": "Malcolm Gladwell",
+    "category": "Statistics",
+    "ISBN": 316346624,
+    "cover": "covers.openlibrary.org/b/isbn/316346624-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  },
+  {
+    "mustRead": 1,
+    "title": "The Little Prince",
+    "author": "Antoine de Saint-Exupery and Richard Howard",
+    "category": "Fiction",
+    "ISBN": 156012197,
+    "cover": "covers.openlibrary.org/b/isbn/156012197-M.jpg",
+    "description": "",
+    "amazonLink": ""
+  }
+]
