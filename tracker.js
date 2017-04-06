a004d3596daf0d3f8376379a66288d248eaa4e44
Philip Young <philip.young.gunawan@gmail.com>
2017-04-06 10:23:13 +0700

    book now clickable

--- tracker.js
+++ tracker.js
@@ -1,5 +1,14 @@
 /* eslint-disable */
 
+// Google Analytics
+// =======================================
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-51658519-1', 'auto');
+ga('send', 'pageview');
+
 // Hotjar Tracking Code for www.philipyoungg.com
 // =======================================
 (function(h,o,t,j,a,r){
