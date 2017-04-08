66eead94fa80fa54b5bbd540e071038d2acf4890
Philip Young <philip.young.gunawan@gmail.com>
2017-04-08 18:13:11 +0700

    personal website

--- bundle.js
+++ bundle.js
@@ -1,7 +1,28 @@
 (function(anime, History, $){
+  window.twttr = (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0],
+    t = window.twttr || {};
+  if (d.getElementById(id)) return t;
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "https://platform.twitter.com/widgets.js";
+  fjs.parentNode.insertBefore(js, fjs);
+
+  t._e = [];
+  t.ready = function(f) {
+    t._e.push(f);
+  };
+
+  return t;
+  }(document, "script", "twitter-wjs"));
+
+
   var updateScript = function() {
-    // externalLinkToNewTab()
     var urlArray = window.location.href.split('/')
+    twttr.widgets.load()
+    if (urlArray[3] !== 'books') {
+      externalLinkToNewTab()
+    }
     if (urlArray[3] === 'thoughts' && urlArray[4]) {
       if (window.DISQUS) {
         DISQUS.reset({
